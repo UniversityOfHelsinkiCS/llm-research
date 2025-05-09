f90947d6fc188651cc82f4cf3c42eef665c4dc99
sutigit <panyanilben@gmail.com>
2025-05-09 21:03:19 +0300

    tunkkaus

--- rag/src/llm/chat.ts
+++ rag/src/llm/chat.ts
@@ -1,7 +1,173 @@
-const main = () => {};
+import type { Assistant } from "openai/resources/beta/assistants";
+import OpenAIService from "./openai/OpenAIService.ts";
+import readline from "readline";
+
+const oapi = new OpenAIService();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function printCommands() {
+  console.log("=========================================");
+  console.log("");
+  console.log("CHAT COMMANDS 💬 ---------------------------");
+  console.log("s - start chat");
+  console.log("l - list chats");
+  console.log("d - delete chat");
+  console.log("o - open chat");
+  console.log("");
+
+  console.log("OPENAI COMMANDS 👾 ---------------------------");
+  console.log("la - list assistants");
+  console.log("lai - list assistants with detailed info");
+  console.log("");
+
+  console.log("OTHER COMMANDS 🫧 ---------------------------");
+  console.log("i - show info");
+  console.log("c - list commands");
+  console.log("q - quit");
+  console.log("");
+}
+
+async function command() {
+  rl.question("Command: ", (answer) => {
+    console.log("");
+
+    switch (answer) {
+      // Chat commands -----------------------------------------------------------------------------------
+
+      case "s": // start chat
+        console.log("Starting chat...");
+        break;
+
+      case "l": // list chats
+        break;
+
+      case "d": // delete chat
+        break;
+
+      case "o": // open chat
+        break;
+
+      // START DISCUSSION IN ROOM
+      // case "sr":
+      //   rl.question("Room ID: ", async (roomId) => {
+      //     const roomrun = new RoomRunner();
+
+      //     roomrun
+      //       .on("discussionStarted", () => {
+      //         console.log("Discussion started! ✨🚀");
+      //         console.log("");
+      //       })
+      //       .on("nextSpeakerTurn", (assistantId: string) => {
+      //         console.log("Next speaker ⏭️");
+      //         console.log("");
+      //       })
+      //       .on(
+      //         "roomTextCreated",
+      //         (data: { assistantId: string; text: Text }) => {
+      //           console.log("🤖 id: " + data.assistantId);
+      //         }
+      //       )
+      //       .on("roomTextDelta", (data: { textDelta: TextDelta }) => {
+      //         process.stdout.write(data.textDelta.value || "");
+      //       })
+      //       .on("roomTextEnded", () => {
+      //         console.log("");
+      //       })
+      //       .on("speakerTurnEnd", () => {
+      //         console.log("");
+      //       })
+      //       .on("discussionEnded", () => {
+      //         console.log("Discussion ended ⛳");
+      //         console.log("");
+      //       })
+      //       .on("error", (err: any) => {
+      //         console.error("Error in discussion 💀💀💀");
+      //         console.log("");
+      //       });
+
+      //     await roomrun.startDiscussion(roomId);
+
+      //     console.log("");
+      //     command();
+      //   });
+      //   break;
+
+      // OpenAI commands  -----------------------------------------------------------------------------------
+
+      case "la": // list assistants
+        (async () => {
+          const assistants = await oapi.getAssistants(10);
+          console.log("Assistants:");
+          assistants.forEach((assistant: Assistant) => {
+            console.log(`Assistant ID: ${assistant.id}`);
+            console.log(`Name: ${assistant.name}`);
+            console.log("");
+          });
+          command();
+        })();
+        break;
+
+      case "lai": // list assistants with detailed info
+        (async () => {
+          const assistants = await oapi.getAssistants(10);
+          console.log("Assistants:");
+          assistants.forEach((assistant: Assistant) => {
+            console.log(`Assistant ID: ${assistant.id}`);
+            console.log(`Name: ${assistant.name}`);
+            console.log(`Created at: ${assistant.created_at}`);
+            console.log(`Description: ${assistant.description}`);
+            console.log(`Model: ${assistant.model}`);
+            console.log(`Instructions: ${assistant.instructions}`);
+            console.log(`Tools: ${JSON.stringify(assistant.tools, null, 2)}`);
+            console.log(
+              `Tool resources: ${JSON.stringify(
+                assistant.tool_resources,
+                null,
+                2
+              )}`
+            );
+            console.log(
+              `Metadata: ${JSON.stringify(assistant.metadata, null, 2)}`
+            );
+            console.log(`Top P: ${assistant.top_p}`);
+            console.log(`Temperature: ${assistant.temperature}`);
+            console.log(`Response format: ${assistant.response_format}`);
+            console.log("");
+          });
+          command();
+        })();
+        break;
+
+      // Other commands -----------------------------------------------------------------------------------
+
+      case "i": // show info
+        console.log("No info for you my friend");
+        command();
+        break;
+
+      case "lc": // list commands
+        printCommands();
+        command();
+        break;
+
+      case "q": // quit
+        process.exit(0);
+
+      default: // invalid command
+        console.log("Invalid command");
+        console.log("");
+        command();
+    }
+  });
+}
 
 const startChat = () => {
-  console.log("Chat started");
+  printCommands();
+  command();
 };
 
 export { startChat };
---
+++ rag/src/llm/openai/OpenAIService.ts
@@ -0,0 +1,186 @@
+import { getAzureOpenAIClient } from "../azure.ts";
+import type OpenAI from "openai";
+import fs from "fs";
+import type { Thread } from "openai/resources/beta/threads/threads";
+import type { Assistant } from "openai/resources/beta/assistants.mjs";
+
+export default class OpenAIService {
+  openai: OpenAI;
+  path: string;
+
+  constructor() {
+    this.openai = getAzureOpenAIClient();
+    this.path = "./src/llm/openai/data/threads.json";
+  }
+
+  // ASSISTANT HANDLING -------------------------------------------------------------------
+  getAssistants = async (limit: number): Promise<Assistant[]> => {
+    const assistants = await this.openai.beta.assistants.list({
+      limit,
+      order: "desc",
+    });
+    return assistants.data;
+  };
+
+  // THREAD HANDLING ----------------------------------------------------------------------
+  saveThread = (thread: Thread) => {
+    const newEntry = {
+      id: thread.id,
+      created_at: thread.created_at,
+    };
+
+    // Read existing file (or start with an empty array if file doesn't exist)
+    let existingData = [];
+    if (fs.existsSync(this.path)) {
+      const fileContent = fs.readFileSync(this.path, "utf-8");
+      try {
+        existingData = JSON.parse(fileContent);
+        if (!Array.isArray(existingData)) existingData = [];
+      } catch (err) {
+        console.error("Invalid JSON, resetting to empty array.");
+        existingData = [];
+      }
+    }
+
+    // Add new entry
+    existingData.push(newEntry);
+
+    // Write updated array back to file
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(existingData, null, 2));
+      return true;
+    } catch (err) {
+      console.error("Error writing file:", err);
+      return false;
+    }
+  };
+
+  deleteThread = async (threadId: string) => {
+    if (!fs.existsSync(this.path)) {
+      console.error("File does not exist.");
+      return false;
+    }
+
+    let threads;
+    try {
+      const fileContent = fs.readFileSync(this.path, "utf-8");
+      threads = JSON.parse(fileContent);
+
+      if (!Array.isArray(threads)) {
+        console.error("JSON is not an array.");
+        return false;
+      }
+    } catch (err) {
+      console.error("Error reading or parsing file:", err);
+      return false;
+    }
+
+    // Filter out the thread to be deleted
+    const updatedThreads = threads.filter((thread) => thread.id !== threadId);
+
+    // Write updated array back and delete the thread
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(updatedThreads, null, 2));
+    } catch (err) {
+      console.error("Error writing file:", err);
+      return false;
+    }
+
+    // delete from openai
+    try {
+      await this.openai.beta.threads.del(threadId);
+      console.log("Thread deleted from OpenAI successfully!");
+    } catch (err) {
+      console.error("Error deleting thread from OpenAI:", err);
+    }
+
+    return true;
+  };
+
+  getThreads = () => {
+    if (!fs.existsSync(this.path)) {
+      console.warn("No threads file found.");
+      return [];
+    }
+
+    try {
+      const fileContent = fs.readFileSync(this.path, "utf-8");
+      const threads = JSON.parse(fileContent);
+
+      if (!Array.isArray(threads)) {
+        console.error("Invalid format: threads.json should contain an array.");
+        return [];
+      }
+
+      return threads;
+    } catch (err) {
+      console.error("Error reading or parsing threads file:", err);
+      return [];
+    }
+  };
+
+  createThread = async () => {
+    const thread = await this.openai.beta.threads.create();
+    const success = this.saveThread(thread);
+    if (success) return thread;
+    else {
+      await this.openai.beta.threads.del(thread.id);
+      console.error("Failed to save thread locally, deleted from OpenAI.");
+      return null;
+    }
+  };
+
+  // MESSAGE HANDLING -----------------------------------------------------------------
+  addMessageToThread = async (threadId: string, content: string) => {
+    try {
+      return await this.openai.beta.threads.messages.create(threadId, {
+        role: "user",
+        content,
+      });
+    } catch (err) {
+      console.error("Error adding message to thread");
+      return null;
+    }
+  };
+
+  getMessagesFromThread = async (threadId: string) => {
+    try {
+      const threadMessages = await this.openai.beta.threads.messages.list(
+        threadId,
+        {
+          limit: 10,
+          order: "asc",
+        }
+      );
+
+      return threadMessages.data.map((message) => ({
+        id: message.id,
+        role: message.role,
+        content: message.content,
+        assistantId: message.assistant_id,
+      }));
+    } catch (err) {
+      console.error("Error fetching messages from thread");
+      return null;
+    }
+  };
+
+  // RUN HANDLING -----------------------------------------------------------------
+  createRun = (threadId: string, assistantId: string) => {
+    try {
+      return this.openai.beta.threads.runs.stream(threadId, {
+        assistant_id: assistantId,
+        metadata: {
+          assistant_id: assistantId,
+        },
+      });
+    } catch (err) {
+      console.error("Error creating run");
+      return null;
+    }
+  };
+
+  cancelRun = async (threadId: string, runId: string) => {
+    return await this.openai.beta.threads.runs.cancel(threadId, runId);
+  };
+}
